b0b264fbb516b15e603d26b08e338455de40027e
BlueCompany11 <gronczewskim@gmail.com>
2024-06-23 16:36:17 +0200

    Added new type of badge on dashboard view

---
+++ Mneme/Mneme.PrismModule.Dashboard/Views/DashboardBadgeTextFirstControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mneme.PrismModule.Dashboard.Views
+{
+	/// <summary>
+	/// Interaction logic for DashboardBadgeTextFirstControl.xaml
+	/// </summary>
+	public partial class DashboardBadgeTextFirstControl : UserControl
+	{
+		public DashboardBadgeTextFirstControl()
+		{
+			InitializeComponent();
+		}
+		public string Header
+		{
+			get => (string)GetValue(HeaderProperty);
+			set => SetValue(HeaderProperty, value);
+		}
+
+		public static readonly DependencyProperty HeaderProperty =
+			DependencyProperty.Register("Header", typeof(string), typeof(DashboardBadgeTextFirstControl), new PropertyMetadata(string.Empty));
+
+		public string Description
+		{
+			get => (string)GetValue(DescriptionProperty);
+			set => SetValue(DescriptionProperty, value);
+		}
+
+		public static readonly DependencyProperty DescriptionProperty =
+			DependencyProperty.Register("Description", typeof(string), typeof(DashboardBadgeTextFirstControl), new PropertyMetadata(string.Empty));
+	}
+}
