92fd639b6f2f5085db71c5cfb09a78c65d2f2ac9
BlueCompany11 <gronczewskim@gmail.com>
2024-06-26 16:03:59 +0200

    Removed hardcoded navigations but 1 place

---
+++ Mneme/Mneme.PrismModule.Integration.Facade/NoteToPreviewNavigator.cs
@@ -0,0 +1,36 @@
+﻿using Mneme.Integrations.GoogleBooks.Contract;
+using Mneme.Integrations.Mneme.Contract;
+using Mneme.Integrations.Pluralsight.Contract;
+using Mneme.Model.Preelaborations;
+using Mneme.PrismModule.Integrations.GoogleBooks.Views;
+using Mneme.PrismModule.Integrations.Mneme.Views;
+using Mneme.PrismModule.Integrations.Pluralsight.Views;
+using Mneme.Views.Base;
+using Prism.Regions;
+
+namespace Mneme.PrismModule.Integration.Facade
+{
+	public class NoteToPreviewNavigator
+	{
+		private readonly IRegionManager regionManager;
+		public NoteToPreviewNavigator(IRegionManager regionManager)
+		{
+			this.regionManager = regionManager;
+		}
+		public void NavigateToPreview(Preelaboration note, NavigationParameters para, string regionName)
+		{
+			if (note is GoogleBooksPreelaboration)
+			{
+				regionManager.RequestNavigate(regionName, nameof(GoogleBooksNotePreviewView), para);
+			}
+			else if (note is PluralsightPreelaboration)
+			{
+				regionManager.RequestNavigate(regionName, nameof(PluralsightNotePreviewView), para);
+			}
+			else if (note is MnemePreelaboration)
+			{
+				regionManager.RequestNavigate(regionName, nameof(MnemeNotePreviewView), para);
+			}
+		}
+	}
+}
