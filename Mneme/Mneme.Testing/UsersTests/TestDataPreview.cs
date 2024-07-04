6e23205c8f92d07cc9e45d76a9232beef6305f77
BlueCompany11 <gronczewskim@gmail.com>
2024-07-04 20:03:11 +0200

    Minor changes

--- Mneme/Mneme.Testing/UsersTests/TestDataPreview.cs
+++ Mneme/Mneme.Testing/UsersTests/TestDataPreview.cs
@@ -3,8 +3,6 @@
 	public class TestDataPreview
 	{
 		public string Title { get; set; }
-		public string Tags { get; set; }
-		public string Status { get; set; }
 		public DateTime CreationTime { get; set; }
 		public string Type { get; set; }
 	}
