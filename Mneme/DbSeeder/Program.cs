6a16b7f01aff80a386240ea7304ee37cc9aa0786
BlueCompany11 <gronczewskim@gmail.com>
2024-07-11 22:45:17 +0200

    updated and optimized dbseeder

--- Mneme/DbSeeder/Program.cs
+++ Mneme/DbSeeder/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Bogus;
+﻿using Bogus;
 using Mneme.Integrations.Mneme.Contract;
 using Mneme.Model.TestCreation;
 using Mneme.Testing.Contracts;
@@ -23,36 +22,61 @@ namespace DbSeeder
 			int amountOfTestsPerNoteMax = 2;
 			int amountOfAnswersInMultipleChoiceTest = 6;
 
-			for (int i = 0 ; i < amountOfSources ; i++)
-			{
-				var source = new Faker<MnemeSource>()
+			var fakeSource = new Faker<MnemeSource>()
 						.RuleFor(s => s.Title, f => f.Random.Words())
-						.RuleFor(s => s.CreationTime, f => f.Date.Past())
+						.RuleFor(s => s.CreationTime, f => f.Date.Between(DateTime.Now.AddDays(-10), DateTime.Now))
 						.RuleFor(s => s.Active, f => f.Random.Bool())
 						.RuleFor(s => s.Details, f => f.Internet.Url())
-						.RuleFor(s => s.IntegrationId, (f, s) => MnemeSource.GenerateIntegrationId(s.Title, s.Details))
-						.Generate();
+						.RuleFor(s => s.IntegrationId, (f, s) => MnemeSource.GenerateIntegrationId(s.Title, s.Details));
+
+			var fakeNote = new Faker<MnemeNote>()
+						.RuleFor(n => n.Title, f => f.Random.Words())
+						.RuleFor(n => n.CreationTime, f => f.Date.Past())
+						.RuleFor(n => n.Content, f => f.Random.Words())
+						.RuleFor(n => n.IntegrationId, f => f.Random.Guid().ToString())
+						.RuleFor(n => n.Path, f => f.Internet.Url());
+
+			var fakeShortTest = new Faker<TestShortAnswer>()
+							.RuleFor(t => t.Created, f => f.Date.Past())
+							.RuleFor(t => t.Importance, f => f.Random.Number(0, 2))
+							.RuleFor(t => t.Hint, f => f.Random.Words())
+							.RuleFor(t => t.Question, f => f.Random.Words())
+							.RuleFor(t => t.Answer, f => f.Random.Word());
+
+			var fakeAnswer = new Faker<TestMultipleChoice>()
+							.RuleFor(t => t.Answer, f => f.Random.Word())
+							.RuleFor(t => t.IsCorrect, f => f.Random.Bool());
+
+			var fakeCorrectAnswer = new Faker<TestMultipleChoice>()
+				.RuleFor(t => t.Answer, f => f.Random.Word())
+				.RuleFor(t => t.IsCorrect, f => true);
+
+			var fakeMultipleChoicesTest = new Faker<TestMultipleChoices>()
+					.RuleFor(t => t.Created, f => f.Date.Past())
+					.RuleFor(t => t.Importance, f => f.Random.Number(0, 2))
+					.RuleFor(t => t.Question, f => f.Random.Words());
+
+			//populate
+			for (int i = 0 ; i < amountOfSources ; i++)
+			{
+				var source = fakeSource.Generate();
 				sources.Add(source);
 				repository.CreateSource(source);
 			}
+			Console.WriteLine("Sources generated");
 			Random random = new Random();
 			foreach (var source in sources)
 			{
 				int amountOfNotes = random.Next(amountOfNotesPerSourceMin, amountOfNotesPerSourceMax + 1);
 				for (int i = 0 ; i < amountOfNotes ; i++)
 				{
-					var note = new Faker<MnemeNote>()
-						.RuleFor(n => n.Title, f => f.Random.Words())
-						.RuleFor(n => n.CreationTime, f => f.Date.Past())
-						.RuleFor(n => n.Content, f => f.Random.Words())
-						.RuleFor(n => n.IntegrationId, f => f.Random.Guid().ToString())
-						.RuleFor(n => n.Path, f => f.Internet.Url())
-						.RuleFor(n => n.Source, f => source)
-						.Generate();
+					var note = fakeNote.Generate();
+					note.Source = source;
 					notes.Add(note);
 					repository.CreateNote(note);
 				}
 			}
+			Console.WriteLine("Notes generated");
 			foreach (var note in notes)
 			{
 				int amountOfTests = random.Next(amountOfTestsPerNoteMin, amountOfTestsPerNoteMax + 1);
@@ -60,47 +84,30 @@ namespace DbSeeder
 				{
 					if (random.Next(0, 2) == 0)
 					{
-						var test = new Faker<TestShortAnswer>()
-							.RuleFor(t => t.Created, f => f.Date.Past())
-							.RuleFor(t => t.Importance, f => f.Random.Number(0, 2))
-							.RuleFor(t => t.Hint, f => f.Random.Words())
-							.RuleFor(t => t.Question, f => f.Random.Words())
-							.RuleFor(t => t.Answer, f => f.Random.Word())
-							.RuleFor(t => t.NoteId, f => note.Id)
-							.Generate();
+						var test = fakeShortTest.Generate();
+						test.NoteId = note.Id;
 						testingRepository.CreateTest(test);
 					}
 					else
 					{
 						var answers = new List<TestMultipleChoice>();
-
 						//add at least one correct answer
-						var corrcetAnswer = new Faker<TestMultipleChoice>()
-								.RuleFor(t => t.Answer, f => f.Random.Word())
-								.RuleFor(t => t.IsCorrect, f => true);
-						answers.Add(corrcetAnswer);
+						answers.Add(fakeCorrectAnswer.Generate());
 
 						for (int j = 0 ; j < amountOfAnswersInMultipleChoiceTest ; j++)
 						{
-							var answer = new Faker<TestMultipleChoice>()
-								.RuleFor(t => t.Answer, f => f.Random.Word())
-								.RuleFor(t => t.IsCorrect, f => f.Random.Bool());
-							answers.Add(answer);
+							answers.Add(fakeAnswer.Generate());
 						}
-
-						var test = new Faker<TestMultipleChoices>()
-							.RuleFor(t => t.Created, f => f.Date.Past())
-							.RuleFor(t => t.Importance, f => f.Random.Number(0, 2))
-							.RuleFor(t => t.Question, f => f.Random.Words())
-							.RuleFor(t => t.NoteId, f => note.Id)
-							.RuleFor(t => t.Answers, f => answers);
+						var test = fakeMultipleChoicesTest.Generate();
+						test.NoteId = note.Id;
+						test.Answers = answers;
 						answers.ForEach(t => t.Test = test);
 
 						testingRepository.CreateTest(test);
 					}
-
 				}
 			}
+			Console.WriteLine("Tests generated");
 		}
 	}
 }
