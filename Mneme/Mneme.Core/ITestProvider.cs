4988e6518d82efe1adf4d980b40a3547e3002dd3
BlueCompany11 <gronczewskim@gmail.com>
2024-07-21 19:00:40 +0200

    refactor dashboard project

--- Mneme/Mneme.Core/ITestProvider.cs
+++ Mneme/Mneme.Core/ITestProvider.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mneme.Model;
 
 namespace Mneme.Core
 {
 	public interface ITestProvider
 	{
-		int GetAllTestsCount();
-		int GetTodaysTests();
+		Task<IReadOnlyList<Test>> GetAllTests();
+		Task<IReadOnlyList<Test>> GetTestsForToday();
 	}
 }
--- Mneme/Mneme.Dashboard/StatisticsProvider.cs
+++ Mneme/Mneme.Dashboard/StatisticsProvider.cs
@@ -18,7 +18,7 @@ namespace Mneme.Dashboard
 		public async Task<int> GetKnownNotesCount(CancellationToken ct = default) => (await integration.GetKnownNotes().ConfigureAwait(false)).Count;
 		public async Task<string?> GetMostRecentSource(CancellationToken ct = default) => (await integration.GetKnownSources().ConfigureAwait(false)).OrderBy(x => x.CreationTime).Select(x => x.Title).FirstOrDefault();
 		public async Task<string?> GetMostRecentNote(CancellationToken ct = default) => (await integration.GetKnownNotes().ConfigureAwait(false)).OrderBy(x => x.CreationTime).Select(x => x.Title + Environment.NewLine + x.Content).FirstOrDefault();
-		public async Task<int> GetAllTestsCount(CancellationToken ct = default) => (await Task.Run(() => testProvider.GetAllTestsCount()));
-		public async Task<int> GetAllTestsForTestingCount(CancellationToken ct = default) => (await Task.Run(() => testProvider.GetTodaysTests()));
+		public async Task<int> GetAllTestsCount(CancellationToken ct = default) => (await Task.Run(() => testProvider.GetAllTests())).Count;
+		public async Task<int> GetAllTestsForTestingCount(CancellationToken ct = default) => (await Task.Run(() => testProvider.GetTestsForToday())).Count;
 	}
 }
